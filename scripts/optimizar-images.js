cf99ddc183965759b3487f2bee3b2b9413d24d41
johanatoledo1990@gmail.com <johanatoledo1990@gmail.com>
2026-09-12 01:07:22 -0500

    nuevas imagenes agregadas

--- scripts/optimizar-images.js
+++ scripts/optimizar-images.js
@@ -3,7 +3,7 @@ import path from "path";
 import sharp from "sharp";
 
 const inputDir = path.resolve("./public");
-const outputDir = path.resolve("./assets-optimized");
+const outputDir = path.resolve("./public-optimized");
 
 const imageExtensions = [".jpg", ".jpeg", ".png"];
 
